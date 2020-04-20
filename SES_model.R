cdd7f85a6bea0a101e11c14751bd1ad6e6326a68
Tom Kraft <kraft.tom@gmail.com>
2020-04-20 17:49:26 -0500

    First round of output for others to use, plus SES network animation

---
+++ SES_model.R
@@ -0,0 +1,63 @@
+######## SES ##########
+# 1) Network with SES
+# initialize empty network with desired attributes
+net <- network.initialize(n = 200, directed = FALSE)
+net <- set.vertex.attribute(net, "SES", rep(0:1, each = 100))
+
+# Setting model parameters
+# NOTE: this is where we can add attributes that will define structure in the way we like based on an ERGM formula
+formation <- ~edges + nodematch("SES", diff=TRUE) + nodefactor("SES") 
+target.stats <- c(150, 30, 15, 20) # correspond to formation formula above
+
+# because networks generated are dynamic, we also set parameters to determine the rate of dissolution of ties
+coef.diss <- dissolution_coefs(dissolution = ~offset(edges) + offset(nodematch("SES", diff=TRUE)), 
+                               duration = c(1, 10, 10))
+
+# Fitting model with desired parameters
+mod1 <- netest(net, formation, target.stats, coef.diss, edapprox = T)
+
+
+# Simulate epidemic
+param <- param.net(inf.prob = 0.3, act.rate = 1, rec.rate=.05)
+status.vector <- rbinom(200, 1, 0.03)   # seed initial infected individuals
+status.vector <- ifelse(status.vector == 1, "i", "s")
+init <- init.net(status.vector = status.vector)
+
+# Structural model features
+control <- control.net(type = "SIR", nsteps = 300, nsims = 10, epi.by = "SES", ncores=2)
+
+# simulate!
+sim1 <- netsim(mod1, param, init, control)
+
+#### WHAT CAN WE EXTRACT FROM THE "sim1" OBJECT ?? ####
+
+
+
+
+# The rest is just plotting
+# Plots of how the epidemic spread
+plot(sim1, mean.line = FALSE, qnts = FALSE, sim.lines = TRUE)
+
+# Plots of how the simulated dynamic networks looked at different timepoints
+par(mfrow = c(1,2), mar = c(0,0,1,0))
+plot(sim1, type = "network", at = 1, col.status = TRUE,
+     main = "Prevalence at t1")
+plot(sim1, type = "network", at = 50, col.status = TRUE,
+     main = "Prevalence at t100")
+
+
+# plot networks at specific points showing race
+nw <- get_network(sim1, sim = 1)
+out <- network.extract(nw, at=20)
+plot(out, vertex.col="SES")
+
+# Make an animated plot of the networks over a specific duration
+slice.par<-list(start=1, end=30, interval=1, aggregate.dur=1,rule="latest")
+compute.animation(nw,slice.par=slice.par)
+
+render.d3movie(nw,
+               filename="SES_network",
+               edge.col="darkgray",displaylabels=TRUE,
+               label.cex=.6,label.col="blue",
+               output.mode = 'htmlWidget',
+               vertex.col="SES")
--- epidemicERGM.R
+++ epidemicERGM.R
@@ -6,13 +6,14 @@ library(ndtv)
 
 # 1) Network with homophily only by race
 # initialize empty network with desired attributes
-net <- network.initialize(n = 30, directed = FALSE)
-net <- set.vertex.attribute(net, "race", rep(0:2, each = 10))
+net <- network.initialize(n = 120, directed = FALSE)
+net <- set.vertex.attribute(net, "race", rep(0:2, each = 30))
 
 # Setting model parameters
 # NOTE: this is where we can add attributes that will define structure in the way we like based on an ERGM formula
-formation <- ~edges + nodefactor("race") + nodematch("race")
-target.stats <- c(30, 15, 30, 26) # correspond to formation formula above
+formation <- ~edges + nodematch("race")
+formation <- ~edges
+target.stats <- c(100) # correspond to formation formula above
 
 # because networks generated are dynamic, we also set parameters to determine the rate of dissolution of ties
 coef.diss <- dissolution_coefs(dissolution = ~offset(edges), duration = 25)
@@ -29,13 +30,13 @@ plot(dx)
 
 
 # Simulate epidemic
-param <- param.net(inf.prob = 0.03, act.rate = 1)
-status.vector <- rbinom(30, 1, 0.02)
+param <- param.net(inf.prob = 0.4, act.rate = 1, rec.rate=.05)
+status.vector <- rbinom(120, 1, 0.05)   # seed initial infected individuals
 status.vector <- ifelse(status.vector == 1, "i", "s")
 init <- init.net(status.vector = status.vector)
 
 # Structural model features
-control <- control.net(type = "SI", nsteps = 500, nsims = 10, epi.by = "race", ncores=2)
+control <- control.net(type = "SIR", nsteps = 200, nsims = 10, epi.by = "race", ncores=2)
 
 # simulate!
 sim1 <- netsim(mod1, param, init, control)
@@ -47,24 +48,24 @@ plot(sim1, mean.line = FALSE, qnts = FALSE, sim.lines = TRUE)
 par(mfrow = c(1,2), mar = c(0,0,1,0))
 plot(sim1, type = "network", at = 1, col.status = TRUE,
      main = "Prevalence at t1")
-plot(sim1, type = "network", at = 50, col.status = TRUE,
-     main = "Prevalence at t500")
+plot(sim1, type = "network", at = 100, col.status = TRUE,
+     main = "Prevalence at t100")
 
 
 # plot networks at specific points showing race
 nw <- get_network(sim1, sim = 1)
-out <- network.extract(nw, at=1)
+out <- network.extract(nw, at=10)
 plot(out, vertex.col="race")
 
 # Make an animated plot of the networks over a specific duration
-slice.par<-list(start=1, end=20, interval=1, aggregate.dur=1,rule="latest")
+slice.par<-list(start=1, end=10, interval=1, aggregate.dur=1,rule="latest")
 compute.animation(nw,slice.par=slice.par)
 
 render.d3movie(nw,
                edge.col="darkgray",displaylabels=TRUE,
                label.cex=.6,label.col="blue",
                output.mode = 'htmlWidget',
-               vertex.col = "race")
+               col.status=T)
 
 
 
@@ -159,17 +160,100 @@ render.d3movie(nw,
 plot(out, vertex.col="race")
 
 
-par(mfrow=c(3,3))
-for(i in seq(0,5, length.out = 9)){
-  net <- network.initialize(n = 45, directed = FALSE)
-  net <- set.vertex.attribute(net, "race", rep(0:1, each = 25))
-  g.sim <- simulate(net ~ edges + nodematch("race"), coef = c(-5, i))
-  plot(g.sim, main=paste("Homophily = ", i, sep=""))
-}
 
-net <- network.initialize(n = 50, directed = FALSE)
-net <- set.vertex.attribute(net, "race", rep(0:1, each = 25))
 
-g.sim <- simulate(net ~ edges , coef = c(-3))
 
-plot(g.sim, vertex.col="race")
+
+
+
+
+
+
+
+
+
+
+######## SES ##########
+# 1) Network with SES
+# initialize empty network with desired attributes
+net <- network.initialize(n = 200, directed = FALSE)
+net <- set.vertex.attribute(net, "SES", rep(0:1, each = 100))
+
+# Setting model parameters
+# NOTE: this is where we can add attributes that will define structure in the way we like based on an ERGM formula
+formation <- ~edges + nodematch("SES", diff=TRUE) + nodefactor("SES") 
+target.stats <- c(150, 30, 15, 20) # correspond to formation formula above
+
+# because networks generated are dynamic, we also set parameters to determine the rate of dissolution of ties
+coef.diss <- dissolution_coefs(dissolution = ~offset(edges) + offset(nodematch("SES", diff=TRUE)), 
+                               duration = c(1, 10, 10))
+
+# Fitting model with desired parameters
+mod1 <- netest(net, formation, target.stats, coef.diss, edapprox = T)
+
+# Model diagnostics
+dx <- netdx(mod1, nsims = 5, nsteps = 200, dynamic=F)
+par(mar = c(3,3,1,1), mgp = c(2,1,0))
+plot(dx)
+
+
+# Simulate epidemic
+param <- param.net(inf.prob = 0.3, act.rate = 1, rec.rate=.05)
+status.vector <- rbinom(200, 1, 0.03)   # seed initial infected individuals
+status.vector <- ifelse(status.vector == 1, "i", "s")
+init <- init.net(status.vector = status.vector)
+
+# Structural model features
+control <- control.net(type = "SIR", nsteps = 300, nsims = 10, epi.by = "SES", ncores=2)
+
+# simulate!
+sim1 <- netsim(mod1, param, init, control)
+
+# Plots of how the epidemic spread
+plot(sim1, mean.line = FALSE, qnts = FALSE, sim.lines = TRUE)
+
+# Plots of how the simulated dynamic networks looked at different timepoints
+par(mfrow = c(1,2), mar = c(0,0,1,0))
+plot(sim1, type = "network", at = 1, col.status = TRUE,
+     main = "Prevalence at t1")
+plot(sim1, type = "network", at = 50, col.status = TRUE,
+     main = "Prevalence at t100")
+
+
+# plot networks at specific points showing race
+nw <- get_network(sim1, sim = 1)
+out <- network.extract(nw, at=20)
+colrs <- c("gray50", "tomato")
+out %v% "color" <- colrs[out %v% "SES" +1]
+plot(out, vertex.col="SES")
+legend(-90,-10, c("Low SES", "High SES"), pch=21,
+       col="#777777", pt.bg=colrs, pt.cex=2, cex=.8, bty="n", ncol=1)
+# Make an animated plot of the networks over a specific duration
+slice.par<-list(start=1, end=30, interval=1, aggregate.dur=1,rule="latest")
+compute.animation(nw,slice.par=slice.par)
+
+render.d3movie(nw,
+               filename="SES_network",
+               edge.col="darkgray",displaylabels=TRUE,
+               label.cex=.6,label.col="blue",
+               output.mode = 'htmlWidget',
+               vertex.col="SES")
+
+
+
+
+
+
+
+
+
+
+  net <- network.initialize(n = 44, directed = FALSE)
+  sarah <- data.frame(SES=c(rep("low", 22), rep("rich", 22)))
+  net %v% "SES" <- as.character(sarah$SES)
+  g.sim <- simulate(net ~ edges + nodefactor("SES") + nodematch("SES", diff=T, levels = c("1", "2")), 
+                    coef = c(-1.3, -1, 0.5, -3))
+  plot(g.sim, main=paste("SES = ", i, sep=""), vertex.col="SES")
+
+
+
